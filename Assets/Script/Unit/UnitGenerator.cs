b82e7408bd8b9d98c0f825f0b9fd0973a8ec5647
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-07-22 20:13:35 +0900

    [add]ユニットを生成するクラスを作った

---
+++ Assets/Script/Unit/UnitGenerator.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/// <summary>
+/// ユニットを生成するクラス
+/// </summary>
+public class UnitGenerator : MonoBehaviour
+{
+    /// <summary>
+    /// キャラを生成する関数
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="cell"></param>
+    /// <returns></returns>
+    public Unit Spawn(CharacterData data, GridCell cell)
+    {
+        if (data == null || data.Prefab == null)
+        {
+            Debug.LogWarning("CharacterData または Unit Prefab が未設定です。");
+            return null;
+        }
+
+        if (cell == null || cell.IsOccupied)
+        {
+            Debug.LogWarning("生成先のセルが無効、または既に埋まっています。");
+            return null;
+        }
+
+        Unit unit = Instantiate(
+            data.Prefab,
+            cell.transform.position,
+            Quaternion.identity);
+
+        if (!unit.Initialize(data, cell))
+        {
+            Destroy(unit.gameObject);
+            return null;
+        }
+
+        return unit;
+    }
+}
