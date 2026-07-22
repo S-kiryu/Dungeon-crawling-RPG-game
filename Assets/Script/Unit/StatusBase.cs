c1dddaddf746247478c0642a3b8082d6b3654f1e
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-07-22 14:35:00 +0900

    [add]ユニットのステータスのベースクラスを作った

---
+++ Assets/Script/Unit/StatusBase.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StatusBase : ScriptableObject
+{
+    public int HP = 0;
+    public int Attack = 0;
+    public int Deffens = 0;
+    public int Speed = 0;
+    public int MoveSpeed = 0;
+    public int Weight = 0;
+}
