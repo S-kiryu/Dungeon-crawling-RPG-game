41d1916d45d4754424fe5c7855668e22e31195fb
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-07-23 13:43:02 +0900

    [add]ステータスをスクリタブルオブジェクトにした

---
+++ Assets/Script/Unit/Status/StatusBase.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+[CreateAssetMenu(menuName = "Battle/Character Status")]
+public class StatusBase : ScriptableObject
+{
+    public int HP = 0;
+    public int Attack = 0;
+    public int Deffens = 0;
+    public int Speed = 0;
+    public int MoveSpeed = 0;
+    public int Weight = 0;
+}
