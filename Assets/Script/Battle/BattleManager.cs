e81f3bc15e25732e4193b21f3947e02c3fd8c384
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-08-20 01:54:51 +0900

    [add]敵のターンに移動するようになった

--- Assets/Script/Battle/BattleManager.cs
+++ Assets/Script/Battle/BattleManager.cs
@@ -1,51 +1,122 @@
+using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// バトルの状態を管理するクラス
+/// </summary>
 public class BattleManager : MonoBehaviour
 {
-    public BattleState CurrentState { get; private set; } = BattleState.SelectUnit;
+    [SerializeField]
+    private EnemyTurnController _enemyTurnController;
+    [SerializeField]
+    private GridManager _gridManager;
 
+    public BattleState CurrentState { get; private set; }
+        = BattleState.SelectUnit;
+
+    private bool _isChangingTurn;
+
+    /// <summary>
+    /// バトルの状態を変更する
+    /// </summary>
+    /// <param name="nextState"></param>
     public void ChangeState(BattleState nextState)
     {
         CurrentState = nextState;
-        Debug.Log($"BattleState changed: {CurrentState}");
-    }
 
-    public void StartMoveSelect()
-    {
-        ChangeState(BattleState.SelectMoveTarget);
+        Debug.Log(
+            $"BattleState changed: {CurrentState}"
+        );
     }
 
     /// <summary>
-    /// 味方を選んだ後に使うボタン
+    /// 敵のターンを開始する
+    /// </summary>
+    public void StartEnemyTurn()
+    {
+        if (_isChangingTurn)
+            return;
+
+        _gridManager.ClearBattleSelection();
+
+        StartCoroutine(EnemyTurnRoutine());
+    }
+
+    /// <summary>
+    /// 敵のターンを処理するコルーチン
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator EnemyTurnRoutine()
+    {
+        _isChangingTurn = true;
+
+        ChangeState(BattleState.EnemyTurn);
+
+        Debug.Log("敵ターン開始");
+
+        if (_enemyTurnController != null)
+        {
+            yield return _enemyTurnController.ExecuteTurn();
+        }
+        else
+        {
+            // EnemyTurnControllerが未完成でも確認できる
+            yield return new WaitForSeconds(1f);
+        }
+
+        Debug.Log("敵ターン終了");
+
+        ChangeState(BattleState.SelectUnit);
+
+        _isChangingTurn = false;
+    }
+
+    /// <summary>
+    /// 移動ボタンが押されたときの処理
     /// </summary>
     public void OnMoveButton()
     {
-        if (CurrentState != BattleState.SelectUnit)
+        if (CurrentState != BattleState.SetMove)
+        {
+            Debug.LogWarning(
+                "先にキャラを選択してください"
+            );
+
             return;
+        }
 
         ChangeState(BattleState.SelectMoveTarget);
     }
 
     /// <summary>
-    /// 攻撃したらターン終了
+    /// 攻撃ボタンが押されたときの処理
     /// </summary>
     public void OnAttackButton()
     {
-        if (CurrentState != BattleState.SelectAfterMoveCommand)
+        if (CurrentState !=
+            BattleState.SelectAfterMoveCommand)
+        {
             return;
+        }
 
         ChangeState(BattleState.SelectAttackTarget);
     }
 
     /// <summary>
-    /// ターンを終了させたいとき
+    /// 待機ボタンが押されたときの処理
     /// </summary>
     public void OnWaitButton()
     {
-        if (CurrentState != BattleState.SelectAfterMoveCommand && CurrentState != BattleState.SelectMoveTarget && CurrentState != BattleState.SelectAttackTarget)
+        if (CurrentState !=
+                BattleState.SelectAfterMoveCommand &&
+            CurrentState !=
+                BattleState.SelectMoveTarget &&
+            CurrentState !=
+                BattleState.SelectAttackTarget)
+        {
             return;
-        Debug.Log("敵のターンに移行");
-        ChangeState(BattleState.EnemyTurn);
-    }
+        }
 
+        StartEnemyTurn();
+    }
 }
--- Assets/Script/Comand/BattleState.cs
+++ Assets/Script/Comand/BattleState.cs
@@ -1,6 +1,10 @@
+/// <summary>
+/// 各ターンの状態を管理するクラス
+/// </summary>
 public enum BattleState
 {
     SelectUnit,
+    SetMove,
     SelectMoveTarget,
     Moving,
     SelectAfterMoveCommand,
