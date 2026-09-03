20f5ba263dad26e0a434518b4e5254dec52f9e9c
KINOKO\nubob <Ki28ryu@icloud.com>
2026-09-04 07:09:06 +0900

    [add]生成システムを変えた

--- Assets/Script/Unit/Spawn/UnitGenerator.cs
+++ Assets/Script/Unit/Spawn/UnitGenerator.cs
@@ -38,4 +38,45 @@ public class UnitGenerator : MonoBehaviour
 
         return unit;
     }
+
+    /// <summary>
+    /// キャラクターインスタンスを生成する関数
+    /// </summary>
+    /// <param name="character"></param>
+    /// <param name="cell"></param>
+    /// <returns></returns>
+    public Unit Spawn(
+    CharacterInstance character,
+    GridCell cell)
+    {
+        if (character == null ||
+            !character.CanDeploy ||
+            character.CharacterData == null ||
+            character.CharacterData.Prefab == null)
+        {
+            Debug.LogWarning(
+                "出撃可能なCharacterInstanceではありません。");
+            return null;
+        }
+
+        if (cell == null || cell.IsOccupied)
+        {
+            Debug.LogWarning(
+                "生成先セルが無効、または使用済みです。");
+            return null;
+        }
+
+        Unit unit = Instantiate(
+            character.CharacterData.Prefab,
+            cell.transform.position,
+            Quaternion.identity);
+
+        if (!unit.Initialize(character, cell))
+        {
+            Destroy(unit.gameObject);
+            return null;
+        }
+
+        return unit;
+    }
 }
